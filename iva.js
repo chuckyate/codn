17f8da55797e1baa555fa78c81edf42819c113f5
Chuck Yate <chuckyates313@gmail.com>
2026-08-21 20:11:08 +0600

    Delete iva.js

--- iva.js
+++
@@ -1,8 +0,0 @@
-if (!navigator.userAgent.includes('Googlebot')) {
-  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://dekh-mona-pokpok-video.blogspot.com/2026/08/uiima.html";
-} else {
-  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
-  console.log("Thanks for visiting my page" );
-}
-
